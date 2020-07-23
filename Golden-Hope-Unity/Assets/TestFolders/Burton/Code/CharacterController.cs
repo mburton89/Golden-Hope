52d635c893edf71946b5484e883a531b2b25b28f
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-07-23 13:48:14 -0500

    Added Simple Character Controller

---
+++ Golden-Hope-Unity/Assets/TestFolders/Burton/Code/CharacterController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody2D;
+    public float movementSpeed;
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //If it's not the player's turn, exit the function.
+
+        int horizontal = 0;     //Used to store the horizontal move direction.
+        int vertical = 0;       //Used to store the vertical move direction.
+
+        //Check if we are running either in the Unity editor or in a standalone build.
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+        //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+
+        //Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+
+        //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+			
+			//Check if Input has registered more than zero touches
+			if (Input.touchCount > 0)
+			{
+				//Store the first touch detected.
+				Touch myTouch = Input.touches[0];
+				
+				//Check if the phase of that touch equals Began
+				if (myTouch.phase == TouchPhase.Began)
+				{
+					//If so, set touchOrigin to the position of that touch
+					touchOrigin = myTouch.position;
+				}
+				
+				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+				{
+					//Set touchEnd to equal the position of this touch
+					Vector2 touchEnd = myTouch.position;
+					
+					//Calculate the difference between the beginning and end of the touch on the x axis.
+					float x = touchEnd.x - touchOrigin.x;
+					
+					//Calculate the difference between the beginning and end of the touch on the y axis.
+					float y = touchEnd.y - touchOrigin.y;
+					
+					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+					touchOrigin.x = -1;
+					
+					//Check if the difference along the x axis is greater than the difference along the y axis.
+					if (Mathf.Abs(x) > Mathf.Abs(y))
+						//If x is greater than zero, set horizontal to 1, otherwise set it to -1
+						horizontal = x > 0 ? 1 : -1;
+					else
+						//If y is greater than zero, set horizontal to 1, otherwise set it to -1
+						vertical = y > 0 ? 1 : -1;
+				}
+			}
+			
+#endif 
+        if (horizontal != 0 || vertical != 0)
+        {
+            _rigidBody2D.drag = 0f;
+            AttemptMove(horizontal, vertical);
+        }
+        else
+        {
+            _rigidBody2D.drag = 5f;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (_rigidBody2D.velocity.magnitude > (maxSpeed))
+        {
+            _rigidBody2D.velocity = _rigidBody2D.velocity.normalized * (maxSpeed);
+        }
+    }
+
+    void AttemptMove(int xDir, int yDir)
+    {
+        Vector2 direction = new Vector2(xDir, yDir);
+        _rigidBody2D.AddForce(direction * movementSpeed);
+    }
+}
---
+++ Golden-Hope-Unity/Assets/TestFolders/Burton/Code/RoomSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 == need bottom door
+    // 2 == need top door
+    // 3 == need left door
+    // 4 == need right door
+
+    private RoomTemplates _roomTemplates;
+    private int rand;
+    [HideInInspector] public bool spawned;
+
+    private void Start()
+    {
+        spawned = false;
+        _roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", .25f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                //spawn room with BOTTOM door
+                rand = Random.Range(0, _roomTemplates.bottomRooms.Length);
+                Instantiate(_roomTemplates.bottomRooms[rand], transform.position, _roomTemplates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                //spawn room with TOP door
+                rand = Random.Range(0, _roomTemplates.topRooms.Length);
+                Instantiate(_roomTemplates.topRooms[rand], transform.position, _roomTemplates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                //spawn room with LEFT door
+                rand = Random.Range(0, _roomTemplates.leftRooms.Length);
+                Instantiate(_roomTemplates.leftRooms[rand], transform.position, _roomTemplates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                //spawn room with RIGHT door
+                rand = Random.Range(0, _roomTemplates.rightRooms.Length);
+                Instantiate(_roomTemplates.rightRooms[rand], transform.position, _roomTemplates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("hello");
+        if (collision.CompareTag("SpawnPoint"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
