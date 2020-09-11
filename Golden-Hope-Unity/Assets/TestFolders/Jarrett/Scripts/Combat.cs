f7fc4567dc6f8aaf07dd354406237d567e4a57d5
Matt Burton <matthewwburton@gmail.com>
2020-09-10 21:03:56 -0700

    Saved Scripts, oops

--- Golden-Hope-Unity/Assets/TestFolders/Jarrett/Scripts/Combat.cs
+++ Golden-Hope-Unity/Assets/TestFolders/Jarrett/Scripts/Combat.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Combat : MonoBehaviour
 {
+    //Instantiate Variables
+    public int Max_Health;
+    public int Current_Health;
+    public int Dark_Essence;
+    public int Max_Armor;
+    public int Current_Armor;
+
+
+    //Instantiate UI Elements
+    public Canvas Combat_UI; 
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         
     }
